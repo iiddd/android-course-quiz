44d128b051bb393041d5654923df26357ed66b39
Andrey Merkulov <merkulov.andrej@gmail.com>
2023-01-18 00:10:39 +0100

    Removed redundant Constants

--- app/src/main/kotlin/com/iiddd/quiz/Constants.kt
+++ app/src/main/kotlin/com/iiddd/quiz/Constants.kt
@@ -5,9 +5,4 @@ object Constants {
     const val USER_NAME: String = "user_name"
     const val TOTAL_QUESTION: String = "total_name"
     const val CORRECT_ANSWERS: String = "correct_answers"
-
-    fun getQuestions(): ArrayList<Question> {
-        val questionList = ArrayList<Question>()
-        return questionList
-    }
 }
