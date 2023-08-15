3e3185bdf318ab22e90ecb01d79b327aff17704e
Andrey Merkulov <merkulov.andrej@gmail.com>
2023-08-15 22:09:49 +0200

    Added WelcomeViewModelTest

---
+++ app/src/test/java/com/iiddd/quiz/ui/welcome/WelcomeViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.iiddd.quiz.ui.welcome
+
+import com.iiddd.quiz.domain.repository.UserDataRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+class WelcomeViewModelTest {
+
+    private val repository: UserDataRepository = mockk()
+    private val viewModel = WelcomeViewModel(repository)
+
+    @Test
+    fun `When saveUserName is called`() {
+        val username = "TestUser"
+        every { repository.storeUsername(username) } returns Unit
+
+        viewModel.saveUsername(username)
+
+        verify { repository.storeUsername(username) }
+    }
+
+    @Test
+    fun `When getUserName is called`() {
+        val response = "TestUser"
+        every { repository.getUsername() } returns response
+
+        val returned = viewModel.getUserName()
+
+        verify { repository.getUsername() }
+        assert(response == returned)
+    }
+}
