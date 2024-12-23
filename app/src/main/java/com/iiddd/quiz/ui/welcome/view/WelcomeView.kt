f44cfb3c6829000f15102e4cbe573699f2d6ff11
Andrey Merkulov <merkulov.andrej@gmail.com>
2024-12-24 00:44:06 +0100

    Added custom preview to include dark mode + minor adjustments

--- app/src/main/java/com/iiddd/quiz/ui/welcome/view/WelcomeView.kt
+++ app/src/main/java/com/iiddd/quiz/ui/welcome/view/WelcomeView.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.iiddd.quiz.R
